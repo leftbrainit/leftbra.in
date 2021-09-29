74cf471f0cb8bd4b37a757656fdc4fc635efe1ba
georgeymac <51747594+georgeymac@users.noreply.github.com>
2021-09-29 11:14:03 +0100

    Update StatBox.vue

---
+++ src/components/StatBox.vue
@@ -0,0 +1,17 @@
+<template>
+    <Wrapper>
+        <div class="relative flex flex-col">
+            <div class="w-3/5 py-3" id="stat">
+                <div class="h-50 w-50 ">
+
+                </div>
+            </div>
+        </div> 
+    </Wrapper>
+</template>
+
+
+<script lang="ts" setup>
+import {computed} from 'vue'
+
+</script>
