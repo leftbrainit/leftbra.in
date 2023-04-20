a14666bf6b5df9b8bfe11b65006741aaca8a8632
danielbangs <daniel@leftbrain.it>
2023-04-20 13:04:36 +0200

    Update btn id

--- src/components/ContactNewBusinessButton.vue
+++ src/components/ContactNewBusinessButton.vue
@@ -1,6 +1,6 @@
 <template>
-    <div id="schedule-call-button" class="">
-        <a
+    <div  class="">
+        <a  id="schedule-call-button"
             @click="startNewBusinessIntercomConversation"
             :class="`bg-primary-100 text-primary hover:bg-primary-500/20 border-primary hover:shadow-primary-500/30`"
             class="cursor-pointer whitespace-nowrap inline-block transition ease-in-out duration-300 rounded-sm px-5 py-2 text-md border text-lg font-normal  uppercase font-mono    hover:shadow-lg"
